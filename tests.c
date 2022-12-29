4b3334f07f4f410d64fb33f4257ef41c82005673
Sidd <35938702+siddsp02@users.noreply.github.com>
2022-12-28 19:52:47 -0700

    Added tests.

--- tests.c
+++ tests.c
@@ -1,13 +1,43 @@
-// Test file (many more tests will be added later on for each algorithm).
-
+/* Unit tests for sorting algorithms (more will be added for bst and heap algorithms). */
 #include "algorithm.h"
 #include <assert.h>
 
+#define TEST_ARR_SIZE 1000
+
+#define TESTSORTFUNC_INIT(name)                                             \
+    void test_##name() {                                                    \
+        size_t i;                                                           \
+        int arr[TEST_ARR_SIZE];                                             \
+        for (i = 0; i < TEST_ARR_SIZE; ++i)                                 \
+            arr[i] = rand();                                                \
+        name(arr, TEST_ARR_SIZE, sizeof(int), cmpi);                        \
+        for (i = 0; i < TEST_ARR_SIZE-1; ++i)                               \
+            assert(arr[i] <= arr[i+1]);                                     \
+    }
+
+// Convert a non-inplace sorting function to one that is inplace.
+#define MAKE_SORTFUNC_INPLACE(name)                                        \
+    void name##_inplace(void *data, size_t len, size_t size, cmpfunc cmp) {\
+        void *ret = name(data, len, size, cmp);                            \
+        memcpy(data, ret, len * size);                                     \
+        free(ret);                                                         \
+    }
+
+MAKE_SORTFUNC_INPLACE(mergesort);
+
+TESTSORTFUNC_INIT(mergesort_inplace);
+TESTSORTFUNC_INIT(insertsort);
+TESTSORTFUNC_INIT(heapsort);
+TESTSORTFUNC_INIT(selectsort);
+TESTSORTFUNC_INIT(bubblesort);
+TESTSORTFUNC_INIT(quicksort);
+
 int main() {
-    int arr[] = {4, 3, 1, 9, 7, 2};
-    int *arr2 = mergesort(arr, 6, sizeof(int), cmpi); // Mergesort with integer comparison.
-    for (int i = 0; i < 5; ++i)
-        assert(arr2[i] <= arr2[i + 1]);
-    free(arr2);
+    test_mergesort_inplace();
+    test_insertsort();
+    test_heapsort();
+    test_selectsort();
+    test_bubblesort();
+    test_quicksort();
     return 0;
 }
